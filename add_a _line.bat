df903ec11d6033bfbfe50282f8ddbfd9a4b45338
RayFB-13 <74745745+RayFB-13@users.noreply.github.com>
2021-04-20 16:57:00 -0400

    Update add_a _line.bat

--- add_a _line.bat
+++ add_a _line.bat
@@ -1,6 +1,7 @@
 @echo off
 echo type the name of the file you want to edit (+file extention).
 SET /P name=type here:
+cls
 echo type the text you want to add.
 SET /P text=type here:
 echo %text%>>%name%
