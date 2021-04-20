caa0c1ca1eb03b9e7cf9bbbb4957ff7d6348f79f
RayFB-13 <74745745+RayFB-13@users.noreply.github.com>
2021-04-20 16:54:14 -0400

    Update add_or_overrite_a_file.bat

--- add_or_overrite_a_file.bat
+++ add_or_overrite_a_file.bat
@@ -1,6 +1,6 @@
 @echo off
 echo type the name of the file you want to edit (+file extention).
 SET /P name=type here:
-echo type the text you want to type.
+echo type the text you want to add.
 SET /P text=type here:
 echo %text%>%name%
